b2c4f64471cedc3ae3554a63609b0ec0cd16929a
andryushchenkoka <kirill2goo@gmail.com>
2023-02-03 15:56:12 +0300

    5.3 Nested Classes

--- src/main/java/nested_classes/inner_classes/CarInner.java
+++ src/main/java/nested_classes/inner_classes/CarInner.java
@@ -1,4 +1,4 @@
-package chapter5;
+package nested_classes.inner_classes;
 
 // Вложенные классы нужны когда требуется привязать один класс к другому
 
@@ -11,19 +11,28 @@ package chapter5;
  */
 
 // Car - внешний класс
-public class Car {
+public class CarInner {
 
     private String model, color;
     private Engine engine;
 
-    public Car(String model, String color, Engine engine) {
+    public CarInner(String model, String color, int power) {
         this.model = model;
         this.color = color;
+        this.engine = this.new Engine("AE-123", power);
+    }
+
+    public CarInner(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public void setEngine(Engine engine) {
         this.engine = engine;
     }
 
     // Engine - вложенный класс по отношению к Car
-    public static class Engine {
+    public class Engine {
 
         private String model;
         private int power;
@@ -38,16 +47,18 @@ public class Car {
             System.out.println("Завёлся");
         }
     }
+}
 
+class CarInnerTest {
     public static void main(String[] args) {
-
         // Создавая объект nested класса нужно указывать и класс, содержащий его
-        Car.Engine engine1 = new Car.Engine("AE-123", 72);
-        engine1.turnOn();
-        Car car = new Car("Audi", "Red", engine1);
+        //Engine engine1 = new Engine("AE-123", 72);    // так нельзя объявлять объект inner класса
 
-        // Внешний класс может обращаться даже к private полям вложенного класса
-        int engPower = engine1.power;
+        CarInner car = new CarInner("BMW", "blue");
+        CarInner.Engine engine = car.new Engine("AE-145", 128);
+        car.setEngine(engine);
 
+        // можно сразу создать объект внещнего класса и из него создать от вложенного Inner класса
+        CarInner.Engine engine1 = new CarInner("Mercedes", "black").new Engine("AE-999", 250);
     }
 }
